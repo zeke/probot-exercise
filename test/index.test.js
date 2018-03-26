8a331b9cce0c941c6476958896bae125c43178d6
Zeke Sikelianos <zeke@sikelianos.com>
2018-03-26 15:09:57 -0700

    add tests for reportError module

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,53 @@
-// You can import your modules
-// const index = require('../index')
+const reportError = require('../lib/report-error')
+const nock = require('nock')
+const github = require('@octokit/rest')()
+const defaultContext = {
+  log: function () { /* no-op */ },
+  github: github,
+  payload: {
+    repository: {
+      name: 'probot-exercise',
+      owner: {
+        login: 'zeke'
+      }
+    }
+  }
+}
 
-test('that we can run tests', () => {
-  // your real tests go here
-  expect(1 + 2 + 3).toBe(6)
+describe('reportError', () => {
+  test('it is a function', () => {
+    expect(typeof reportError).toBe('function')
+  })
+
+  test('aborts if issue with same title already exists', async () => {
+    const mock = getAllIssuesMock()
+    const opts = {title: 'title 3', body: 'something went wrong'}
+    const result = await reportError(defaultContext, opts)
+    expect(mock.isDone()).toBe(true)
+    expect(result).toBe(null)
+  })
+
+  test('creates issue if title does not already exist', async () => {
+    const mock1 = getAllIssuesMock()
+    const mock2 = nock('https://api.github.com')
+      .post('/repos/zeke/probot-exercise/issues')
+      .reply(200, {yay: true})
+
+    const opts = {title: 'title 4', body: 'something went wrong'}
+    const result = await reportError(defaultContext, opts)
+    expect(mock1.isDone()).toBe(true)
+    expect(mock2.isDone()).toBe(true)
+    expect(result.data.yay).toBe(true)
+  })
 })
 
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
+function getAllIssuesMock () {
+  const res = [
+    {title: 'title 1'},
+    {title: 'title 2'},
+    {title: 'title 3'}
+  ]
+  return nock('https://api.github.com')
+    .get('/repos/zeke/probot-exercise/issues?per_page=100')
+    .reply(200, res)
+}
